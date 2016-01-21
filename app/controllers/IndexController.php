e32a926191fe8a3a8a1d50d6d8d75d1a7216f16b
Ivar Koster <admin@ivarkoster.nl>
2016-01-21 11:25:26 +0100

    trest stuff

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,11 +1,15 @@
 <?php
 
+use Phalcon\Mvc\Model\Criteria;
+
 class IndexController extends ControllerBase
 {
 
     public function indexAction()
     {
+		$query = Measurement::find("measurement_type = 2 order by date");
 
+		var_dump($query);exit;
     }
 
 }
